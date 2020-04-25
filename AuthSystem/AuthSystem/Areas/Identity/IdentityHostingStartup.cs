4cf342192a36eb316821156cb510b6f39bcb8e6f
Rigoberto Estrella Sosa <estrella_rigoberto@hotmail.com>
2020-04-24 20:49:54 -0400

    Creación de Log In en Aps.net Core

---
+++ AuthSystem/AuthSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthSystem.Areas.Identity.IdentityHostingStartup))]
+namespace AuthSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthSystemDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthSystemDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    }
+                )
+                    .AddEntityFrameworkStores<AuthSystemDbContext>();
+            });
+        }
+    }
+}
