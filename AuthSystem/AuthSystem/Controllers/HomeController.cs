d8109c82b4990d453618c8b68c2bfc55cdaf324e
Somsana Ninthavong <ac100551@avcol.school.nz>
2020-09-08 13:16:07 +1200

    Add path way to Department

--- AuthSystem/AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/AuthSystem/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace AuthSystem.Controllers
             return View();
         }
 
+        public IActionResult Department()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
