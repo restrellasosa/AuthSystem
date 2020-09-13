b3e587091020fe1b7e0a5f405ea88d5f594ef57d
Somsana Ninthavong <ac100551@avcol.school.nz>
2020-09-14 10:54:05 +1200

    d

---
+++ AuthSystem/AuthSystem/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+    }
+}
